60392ad223e38cdba86429821e7a52eac6df9def
Chris Skoblenick <chris.skoblenick@ratehub.ca>
2018-08-09 17:54:49 -0400

    remove broken demo files and typing overrides

--- src/demo/EmailInput.ts
+++
@@ -1,8 +0,0 @@
-import TextInput from './TextInput';
-
-export default Object.assign(TextInput.bind(null), TextInput, {
-   validate: (v: string): Promise<string> =>
-      /.+@.+\..+/.test(v)
-         ? Promise.resolve(v)
-         : Promise.reject('Invalid email address'),
-});
--- typings/react-display-name.d.ts
+++
@@ -1,2 +0,0 @@
-declare module 'react-display-name';
-declare module 'prop-types';
